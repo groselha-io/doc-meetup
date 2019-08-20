1d17dfb84dbd6d55cfdceb369703c75cbb3d448a
Luan Castheloge <luanccp@gmail.com>
2019-08-20 14:57:42 -0300

    [ADD] New card with state

---
+++ src/components/card/Imagecardnone.md
@@ -0,0 +1,7 @@
+### How to use this component
+
+```jsx 
+import Imagecardnome from './imagecardnone';
+
+<Imagecardnone state="default" text="R$1.790.000" text2="Rua Arthur de azevedo 359" text3="Pinheiros - 2° andar" img_src="https://content.loft.com.br/homes/7h3fw/banner_thumbnail.jpg" />
+```
---
+++ src/components/card/imagecardnone.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './imagecardnone.css';
+
+
+export default class Imagecardnone extends React.Component {
+  render() {
+    return (
+      <div className="pd-onhover-parent imagecardnone">
+          { (this.props.state === "default") ?
+              <div className="imagecardnone-default-3">
+                  <div className="imagecardnone-0-0-0">
+                      <div className="imagecardnone-rectangle_9">
+                          <div className="imagecardnone-0-0-0-0-0">
+                              <div className="imagecardnone-image_2" style={{"backgroundImage": ("url('"+(this.props.img_src)+"')")}} /> 
+                          </div>
+                          <div className="imagecardnone-0-0-0-0-1">
+                              <div className="imagecardnone-rectangle_3">
+                                  <div className="imagecardnone-0-0-0-0-1-0-0">
+                                      <div className="imagecardnone-r_2">
+                                          { this.props.text }
+                                      </div>
+                                  </div>
+                                  <div className="imagecardnone-0-0-0-0-1-0-1">
+                                      <div className="imagecardnone-rua_batataes_3">
+                                          { this.props.text2 }
+                                      </div>
+                                  </div>
+                                  <div className="imagecardnone-0-0-0-0-1-0-2">
+                                      <div className="imagecardnone-jardim_paulista_1">
+                                          { this.props.text3 }
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          : null}
+          <div className="pd-onhover imagecardnone-_hover-7">
+              <div className="imagecardnone-1-0">
+                  <div className="imagecardnone-rectangle_1">
+                      <div className="imagecardnone-1-0-0-0">
+                          <div className="imagecardnone-rectangle_9-2">
+                              <div className="imagecardnone-1-0-0-0-0-0">
+                                  <div className="imagecardnone-image_2-5" style={{"backgroundImage": ("url('"+(this.props.img_src)+"')")}} /> 
+                              </div>
+                              <div className="imagecardnone-1-0-0-0-0-1">
+                                  <div className="imagecardnone-rectangle_3-6">
+                                      <div className="imagecardnone-1-0-0-0-0-1-0-0">
+                                          <div className="imagecardnone-r_2_">
+                                              { this.props.text }
+                                          </div>
+                                      </div>
+                                      <div className="imagecardnone-1-0-0-0-0-1-0-1">
+                                          <div className="imagecardnone-rua_batataes_33">
+                                              { this.props.text2 }
+                                          </div>
+                                      </div>
+                                      <div className="imagecardnone-1-0-0-0-0-1-0-2">
+                                          <div className="imagecardnone-jardim_paulista_11">
+                                              { this.props.text3 }
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  }
+};
