2402f2059039bdd95b7a639789152e31cf499f73
Luan Castheloge <luanccp@gmail.com>
2019-08-20 15:09:44 -0300

    [CORE] After first build

---
+++ styleguide/precache-manifest.5fedcbb96d1743d2cd2a52baccdb3ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1e077012f357522a6b",
+    "url": "build/2.0a1e0770.js"
+  },
+  {
+    "revision": "7171ce8928d77cc1c522",
+    "url": "build/bundle.7171ce89.js"
+  },
+  {
+    "revision": "e018ca52430334b04c03",
+    "url": "build/main.e018ca52.js"
+  },
+  {
+    "revision": "c45dd48b3db7511a04b2bf98cb4f6f12",
+    "url": "index.html"
+  },
+  {
+    "revision": "e018ca52430334b04c03",
+    "url": "static/css/main.31171ce8.chunk.css"
+  }
+]);
